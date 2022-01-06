c68a45d0f0af358453c2a24708e4c3d9b56deb86
Luiz Miguel <miguelluiz0@outlook.com>
2022-01-06 19:54:13 -0300

    continuacao da funcao start

--- index.js
+++ index.js
@@ -10,25 +10,70 @@ let comandos_atuais = `
 <b>/help</b>:  Ajuda e referências
 `
 
+let vContador_raca = 0;
 
+//ACOES
+
+//ACOES DE START:
+bot.action('elfo', (ctx)=>{
+    ctx.deleteMessage();
+    ctx.reply('A Eruchîn, ú-dano i faelas a hyn an uben tanatha le faelas.')
+    ctx.reply('Agora saia da minha seção, orelha pontuda.')
+    ctx.reply('/comando');
+})
+
+bot.action('anao', (ctx)=>{
+    ctx.reply('');
+})
+
+bot.action('orc', (ctx)=>{
+    ctx.deleteMessage();
+    ctx.reply('Suma!! Suma da minha seção. Agora!!');
+    vContador_raca = 1;
+    return(vContador_raca);
+})
 
 // COMANDOS
 bot.start(async (ctx) =>{
-    ctx.reply('Olá, ' + ctx.from.first_name);
-    await ctx.reply('Eu sou <b>Thorin</b>, filho de Thrain, filho de Thror.', 
-        {
-            parse_mode: 'HTML',
-        }
-    );
-    await ctx.reply('Já lhe adianto que não dividirei meu ouro. Principalmente caso seja um dragão');
-    ctx.reply('Diga-me, qual a sua raça?');
+    if(vContador_raca == 0){
+        ctx.reply('Olá, ' + ctx.from.first_name);
+        await ctx.reply('Eu sou <b>Thorin</b>, filho de Thrain, filho de Thror.', 
+            {
+                parse_mode: 'HTML',
+            }
+        );
+        await ctx.reply('Já lhe adianto que não dividirei meu ouro. Principalmente caso seja um dragão');
+        ctx.reply('Diga-me, qual a sua raça?', {
+            reply_markup:{
+                inline_keyboard:[
+                    [
+                        {text: 'ELFO', callback_data:'elfo'},
+                        {text: 'ANÃO', callback_data: 'anao'},
+                        {text: 'ORC', callback_data: 'orc'}
+                    ], 
+                    [
+                        {text: 'HOBBIT', callback_data:'hobbit'},
+                        {text: 'MAGO', callback_data: 'mago'},
+                        {text: 'TROLL', callback_data: 'troll'}
+                    ]
+                ]
+            }
+        })
+    }
+    else{
+        ctx.reply('Já lhe disse, Orc, suma daqui!! Procure outro comando.')
+    }
 });
+
+
 
 bot.help((ctx)=>{
     ctx.reply('<b><i>Tá perdido, né? Tão toma o tutorial aí:</i></b>' + 
-    `${comandos_atuais}`, {
+    `${comandos_atuais}`,
+    {
         parse_mode: 'HTML',
     })
 })
+
 
 bot.launch();
