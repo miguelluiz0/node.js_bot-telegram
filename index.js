51bff31a7b5e7a739bfcc708474abe4b1eb9aad0
Luiz Miguel <miguelluiz0@outlook.com>
2022-01-07 20:03:28 -0300

    Finalizaçao da Função /start

--- index.js
+++ index.js
@@ -11,25 +11,59 @@ let comandos_atuais = `
 `
 
 let vContador_raca = 0;
+// 0 = Escolher raça,
+// 1 = action_elfo,
+// 2 = action_anao,
+// 3 = action_orc,
+// 4 = action_hobbit,
+// 5 = action_mago = Escolher raça,
+// 6 = action_troll
 
-//ACOES
+//---------------------
+//      ACOES
+//---------------------
 
 //ACOES DE START:
-bot.action('elfo', (ctx)=>{
+bot.action('elfo', async (ctx)=>{
+    vContador_raca = 1;
     ctx.deleteMessage();
-    ctx.reply('A Eruchîn, ú-dano i faelas a hyn an uben tanatha le faelas.')
-    ctx.reply('Agora saia da minha seção, orelha pontuda.')
+    await ctx.reply('A Eruchîn, ú-dano i faelas a hyn an uben tanatha le faelas.')
+    await ctx.reply('Agora saia da minha seção, orelha pontuda.')
     ctx.reply('/comando');
+    return(vContador_raca);
 })
 
 bot.action('anao', (ctx)=>{
-    ctx.reply('');
-})
+    vContador_raca = 2;
+    ctx.deleteMessage();
+    ctx.reply('Então somos quase irmãos. \nOlhe, jovem Anão, se vir um dragão por aí, me avise.');
+    return(vContador_raca);
+}) 
 
 bot.action('orc', (ctx)=>{
+    vContador_raca = 3;
     ctx.deleteMessage();
     ctx.reply('Suma!! Suma da minha seção. Agora!!');
-    vContador_raca = 1;
+    return(vContador_raca);
+})
+bot.action('hobbit', (ctx)=>{
+    vContador_raca = 4;
+    ctx.deleteMessage();
+    ctx.reply('Ora, ora, já o considero um grande amigo. \nApareça outro dia, vou te contar sobre o lendário Hobbit que conseguia montar um cavalo.');
+    return(vContador_raca);
+})
+bot.action('mago', async (ctx)=>{
+    vContador_raca = 0;
+    ctx.deleteMessage();
+    await ctx.reply('Ora, o último mago com quem falei salvou minha vida e meu Reino!!');
+    ctx.reply('Tome um presente: histórias antigas dizem que magos conseguem trocar de raça, tente!! quem sabe é seu dia de sorte.')
+    return(vContador_raca);
+})
+bot.action('troll', async (ctx)=>{
+    vContador_raca = 6;
+    ctx.deleteMessage();
+    await ctx.reply('Um Troll nem sequer saber ler. Jamais escolheria corretamente.');
+    await ctx.reply('Ou está mentindo para mim, ou é o Troll mais inteligente de toda a história.');
     return(vContador_raca);
 })
 
@@ -42,7 +76,7 @@ bot.start(async (ctx) =>{
                 parse_mode: 'HTML',
             }
         );
-        await ctx.reply('Já lhe adianto que não dividirei meu ouro. Principalmente caso seja um dragão');
+        await ctx.reply('Já lhe adianto que não dividirei meu ouro. \nPrincipalmente caso seja um dragão');
         ctx.reply('Diga-me, qual a sua raça?', {
             reply_markup:{
                 inline_keyboard:[
@@ -57,11 +91,32 @@ bot.start(async (ctx) =>{
                         {text: 'TROLL', callback_data: 'troll'}
                     ]
                 ]
-            }
+            }   
         })
     }
+    else if(vContador_raca == 1){
+        await ctx.reply('O que foi, elfo? Já lhe disseram que a raça não pode ser alterada?');
+        ctx.reply('Gloria somente aos anões!!');
+    }
+    else if(vContador_raca == 2){
+        ctx.reply('Por sermos irmãos irei perdoar a tentativa de traição');
+        ctx.reply('Esqueça essa ideia e use /signos \nPor lá anões ganham armaduras de ouro!! hahahaha!!');
+    }
+    else if(vContador_raca == 3){
+        ctx.reply('Já lhe disse, Orc, suma daqui!! Procure outro comando.');
+    }
+    else if(vContador_raca == 4){
+        ctx.reply('Um Hobbit é o que é. Não o que deseja ser.');
+    }
+    else if(vContador_raca == 6){
+        ctx.reply('Então o humano achou que ia me enganar e agora quer trocar de raça? <b>NEGATIVO</b>. \nSuma antes que eu te parta ao meio com meu machado!', 
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Confesse, você nem sequer sabe que Easter Egg é esse.');
+    }
     else{
-        ctx.reply('Já lhe disse, Orc, suma daqui!! Procure outro comando.')
+        ctx.reply('Se chegou até aqui, você fez algo de muito errado.');
     }
 });
 
