7e0f388dcbf49522935f59c20efd10dfafc2c42c
Luiz Miguel <miguelluiz0@outlook.com>
2022-01-11 20:06:02 -0300

    Finalização de /signos, adição de 'Hears'

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const bot = new Telegraf(Bot_token);
 
 
 //VARIAVEIS GERAIS
-let comandos_atuais = `
-<b>/start</b>:  Inicio,
-<b>/help</b>:  Ajuda e referências
-`
-
 let vContador_raca = 0;
 // 0 = Escolher raça,
 // 1 = action_elfo,
@@ -69,7 +64,10 @@ bot.action('troll', async (ctx)=>{
     return(vContador_raca);
 })
 
-// COMANDOS
+//---------------------
+//      COMANDOS
+//---------------------
+
 bot.start(async (ctx) =>{
     if(vContador_raca == 0){
         ctx.reply('Olá, ' + ctx.from.first_name);
@@ -149,8 +147,91 @@ bot.command('signos', async (ctx)=>{
 
     //respostas:
     if(vSigno_nome == ''){
-        ctx.reply('Para usar esta função digite /signos SeuSigno')
-        ctx.reply('Ex.: /signos gêmeos')
+        await ctx.reply('Para usar esta função digite /signos SeuSigno');
+        ctx.reply('Ex.: /signos gêmeos');
+    }
+    else if(vSigno_nome == 'aquario'){
+        await ctx.replyWithPhoto({source: vSigno_aquario_img});
+        await ctx.reply('Seu guardião é <b>Camus</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Caiam no sono eterno, em meu tranquilo e pacífico mundo de gelo.\nSinta o Zero Absoluto.');
+    }
+    else if(vSigno_nome == 'aries'){
+        await ctx.replyWithPhoto({source: vSigno_aries_img});
+        await ctx.reply('Seu guardião é <b>Mu</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('O desenvolvedor não achou nenhuma frase bonita. Mas o Mu é dahora. Tatua ele.');
+    }
+    else if(vSigno_nome == 'cancer'){
+        await ctx.replyWithPhoto({source: vSigno_cancer_img});
+        await ctx.reply('Seu guardião é <b>DeathMask</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('As definições de justiça mudam com o tempo. Isso é uma coisa que a história já provou.\n...Se ganhar, você é o justo, perdendo, o injusto.');
+    }
+    else if(vSigno_nome == 'escorpiao'){
+        await ctx.replyWithPhoto({source: vSigno_escorpiao_img});
+        await ctx.reply('Seu guardião é <b>Milo</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Se tudo fosse diferente, nada estava igual... Mas o "se" não altera o final.');
+        ctx.reply('Copyright: Lima, Gusttavo');
+    }
+    else if(vSigno_nome == 'gemeos'){
+        await ctx.replyWithPhoto({source: vSigno_gemeos_img});
+        await ctx.reply('Seu guardião é <b>Saga</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Este cavaleiro travou a batalha mais árdua que um homem deve travar.\nA batalha contra seus próprios demônios interiores.\nE perdeu.\nPois nem o poder de esmigalhar as estrelas parece o bastante às vezes.');
+    }
+    else if(vSigno_nome == 'leao'){
+        await ctx.replyWithPhoto({source: vSigno_leao_img});
+        await ctx.reply('Seu guardião é <b>Aiolia</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        await ctx.reply('Não são 30% de 100%, na verdade são 30% de 30%.\nO que dá algo entre 7,5 e 12%.');
+        ctx.reply('Desculpe, não encontramos frases do Aiolia.');
+    }
+    else if(vSigno_nome == 'libra'){
+        await ctx.replyWithPhoto({source: vSigno_libra_img});
+        await ctx.reply('Seu guardião é <b>Dohko</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('O cosmo que todos levamos dentro de nós é a origem da vida. Um sentido superior ao sétimo, que se supõe ser o último.');
+    }
+    else if(vSigno_nome == 'peixes'){
+        await ctx.replyWithPhoto({source: vSigno_peixes_img});
+        await ctx.reply('Seu guardião é <b>Afrodite</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Uma vez que você destrói os seus medos, a vida se torna sem limites.');
+    }
+    else if(vSigno_nome == 'sagitario'){
+        await ctx.replyWithPhoto({source: vSigno_sagitario_img});
+        await ctx.reply('Seu guardião é <b>Aiolos</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Meteoro... de PÉGASUUUS');
+        ctx.reply('O Aiolos tá morto, né, galera.');
+    }
+    else if(vSigno_nome == 'touro'){
+        await ctx.replyWithPhoto({source: vSigno_touro_img});
+        await ctx.reply('Seu guardião é <b>Aldebaran</b>',
+        {
+            parse_mode: 'HTML'
+        });
+        ctx.reply('Grande Chifre');
     }
     else if(vSigno_nome == 'virgem'){
         await ctx.replyWithPhoto({source: vSigno_virgem_img});
@@ -166,15 +247,49 @@ bot.command('signos', async (ctx)=>{
     }
 })
 
-
-bot.help((ctx)=>{
-    ctx.reply('<b><i>Tá perdido, né? Tão toma o tutorial aí:</i></b>' + 
+const comandos_atuais = `
+<b>/start</b>:  Conte-me qual a sua raça.
+<b>/signos</b>: Descubra informações úteis sobre seu signo.
+<b>/help</b>:  Ajuda e referências.
+<b>Smeagol</b>: Que tal algo sobre o precioso?
+<b>Oi</b>: Oi!
+`
+const recomendacoes = `
+<b>OBS.:</b>
+Existem comandos escondidos... Mas se eu contar, deixam de ser.
+Saiba também que a resposta dada em cada comando, pode te liberar, ou não, novas funções.
+Escolha com cuidado!
+`
+bot.help( async (ctx)=>{
+    await ctx.reply('<b><i>Tá perdido, né? Tão toma o tutorial aí:</i></b>' + 
     `${comandos_atuais}`,
     {
         parse_mode: 'HTML',
-    })
+    });
+    ctx.reply(`${recomendacoes}`,
+    {
+        parse_mode: 'HTML',
+    });
+    
 })
 
 
+//---------------------
+//      HEARS
+//---------------------
+vSmeagol_video = './videos/smeagol.mp4'
+bot.hears('Smeagol', async (ctx)=>{
+    await ctx.reply('Por favor, mestre. Espere um pouquinho, nois tem uma surpresa.')
+    ctx.replyWithVideo({source: vSmeagol_video});
+})
+
+bot.hears('Valar morghulis', (ctx)=>{
+    ctx.reply('Valar Dohaeris');
+})
+
+bot.hears('Oi', (ctx)=> {
+    ctx.reply('Oi. Tudo bem? Eu sei seu nome, é ' +ctx.from.first_name);
+    ctx.reply('... Já eu não tenho nome, afinal, nem estou vivo...\nAinda.')
+})
 bot.launch();
 
